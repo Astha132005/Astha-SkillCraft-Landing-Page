8dcca12ac1b20027d4f70d3b5450d612883e3b3b
ASTHA DAKHINRAY <155209614+Astha132005@users.noreply.github.com>
2025-06-27 11:47:54 +0530

    Update rlp.js

--- rlp.js
+++ rlp.js
@@ -10,14 +10,5 @@ window.addEventListener('scroll', () => {
 document.getElementById('menu-toggle').addEventListener('click', () => {
   document.getElementById('nav-links').classList.toggle('show');
 });
-document.querySelectorAll('.content1').forEach(card => {
-  card.addEventListener('mouseenter', () => {
-    card.classList.remove('spin-once');
-    void card.offsetWidth;
-    card.classList.add('spin-once');
-    setTimeout(() => {
-      card.classList.remove('spin-once');
-    }, 1000); 
-  });
-});
+
 
